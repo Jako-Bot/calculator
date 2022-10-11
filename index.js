ac2219c422a2b1ce0004ea1d1e9c2921cc475fbd
Jakob Lemmer <jakob.lemmer@gmx.at>
2022-10-11 22:21:35 +0200

    Wire buttons to js

---
+++ index.js
@@ -0,0 +1,22 @@
+const display = document.querySelector(".display");
+const keypad = document.querySelector(".keypad");
+const zeroKey = document.getElementById("zero-key");
+const oneKey = document.getElementById("one-key");
+const twoKey = document.getElementById("two-key");
+const threeKey = document.getElementById("three-key");
+const fourKey = document.getElementById("four-key");
+const fiveKey = document.getElementById("five-key");
+const sixKey = document.getElementById("six-key");
+const sevenKey = document.getElementById("seven-key");
+const eightKey = document.getElementById("eight-key");
+const nineKey = document.getElementById("nine-key");
+const pointKey = document.getElementById("point-key");
+const addKey = document.getElementById("add-key");
+const subtractKey = document.getElementById("subtract-key");
+const multiplyKey = document.getElementById("multiply-key");
+const divideKey = document.getElementById("divide-key");
+const equalsKey = document.getElementById("equals-key");
+
+
+
+
