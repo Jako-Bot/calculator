b03593689270d34037895ef78010bd477c6215f5
Jakob Lemmer <jakob.lemmer@gmx.at>
2022-10-15 23:00:18 +0200

    Add button and functionality for '.' / remove bugs.

--- index.js
+++ index.js
@@ -18,38 +18,45 @@ const multiplyKey = document.getElementById("multiply-key");
 const divideKey = document.getElementById("divide-key");
 const equalsKey = document.getElementById("equals-key");
 const clearKey = document.getElementById("clear-key");
+const backspaceKey = document.getElementById("backspace-key");
 
 
 let displayBuffer = '';
 const operatorsRegex = /[+*÷-]/;
 
-clearKey.addEventListener("click", () => {
-    displayBuffer = '';
-    display.textContent = displayBuffer;
-});
-
-function operate(num1, num2, operator) {
-    switch (operator) {
-        case "+": return Number(num1) + Number(num2);
-        case "-": return num1 - num2;
-        case "*": return num1 * num2;
-        case "÷": return num1 / num2;
-        default: return "Unknown operator";
-    }
-}
-
 function isOperator(char) {
     return (char === '+' || char === '-' || char === '*' || char === '÷');
 }
 
+function operate(num1, num2, operator) {
+    let result = 0;
+    switch (operator) {
+        case "+": return Number(num1) + Number(num2);
+        case "-": return Number(num1) - Number(num2);
+        case "*": return Number(num1) * Number(num2);
+        case "÷":
+            if (Number(num2) == 0) {return "Can't divide by 0";}
+            else {return Number(num1) / Number(num2);}
+        default: return "Unknown operator";
+    }
+}
+
 function processInput() {
-    if (isOperator(this.textContent)
+    if (display.textContent === '' && this.textContent === '-') {
+        displayBuffer = '-';
+        display.textContent = displayBuffer;
+        return;
+    } else if (((display.textContent === ''
+                || display.textContent.length == 1 && isOperator(display.textContent[0]))
+                && isOperator(this.textContent))) {
+        return;
+    } else if (isOperator(this.textContent)
             && isOperator(display.textContent[display.textContent.length-1])) {
         displayBuffer = displayBuffer.substring(0, displayBuffer.length -1) + this.textContent;
         display.textContent = displayBuffer;
-
+        return;
     } else if (isOperator(this.textContent) 
-            && operatorsRegex.test(display.textContent)) {
+            && operatorsRegex.test(display.textContent.slice(1))) {
         let operands = [];
         let result = 0;
         let operator;
@@ -58,17 +65,23 @@ function processInput() {
             operator = "+";            
             operands = displayBuffer.split("+");
             result = operate(operands[0], operands[1], operator);
-        } else if (displayBuffer.includes("-")) {
-            operator = "-";    
-            operands = displayBuffer.split("-");
+        } else if (displayBuffer.includes("÷")) {
+            operator = "÷";    
+            operands = displayBuffer.split("÷");
             result = operate(operands[0], operands[1], operator);
+
         } else if (displayBuffer.includes("*")) {
             operator = "*";    
             operands = displayBuffer.split("*");
             result = operate(operands[0], operands[1], operator);
-        } else if (displayBuffer.includes("÷")) {
-            operator = "÷";    
-            operands = displayBuffer.split("÷");
+        } else if (displayBuffer.includes("-")) {
+            if (displayBuffer[0] === '-') {
+                operands = displayBuffer.slice(1).split("-");
+                operands[0] *= -1;
+            } else {
+                operands = displayBuffer.split("-");
+            }
+            operator = "-";    
             result = operate(operands[0], operands[1], operator);
         } else {
             display.textContent = "Error in processInput()";    
@@ -101,27 +114,37 @@ divideKey.addEventListener("click", processInput);
 
 equalsKey.addEventListener("click", ()=> {
 
+    if (displayBuffer == '') return;
     if (isOperator(display.textContent[display.textContent.length-1])) {
         return;}
 
     let operands = [];
     let result = 0;
 
-    if (!(operatorsRegex.test(display.textContent))
-            || display.textContent === '') return;
+    if (isOperator(displayBuffer[0])) {
+        if (!(operatorsRegex.test(display.textContent.slice(1)))
+                || display.textContent === '') return;
+    } else if (!(operatorsRegex.test(display.textContent))
+    || display.textContent === '') return;
 
     if (displayBuffer.includes("+")) {
         operands = displayBuffer.split("+");
         result = operate(operands[0], operands[1], "+");
-    } else if (displayBuffer.includes("-")) {
-        operands = displayBuffer.split("-");
-        result = operate(operands[0], operands[1], "-");
-    } else if (displayBuffer.includes("*")) {
-        operands = displayBuffer.split("*");
-        result = operate(operands[0], operands[1], "*");
     } else if (displayBuffer.includes("÷")) {
         operands = displayBuffer.split("÷");
         result = operate(operands[0], operands[1], "÷");
+    } else if (displayBuffer.includes("*")) {
+        operands = displayBuffer.split("*");
+        result = operate(operands[0], operands[1], "*");
+    } else if (displayBuffer.includes("-")) {
+        if (displayBuffer[0] === "-"){  //negative first number
+            displayBuffer = displayBuffer.slice(1);
+            operands = displayBuffer.split("-");
+            operands[0] *= -1;
+        } else {
+            operands = displayBuffer.split("-");
+        }
+        result = operate(operands[0], operands[1], "-");
     } else {
         display.textContent = "Error! No operator!"
         return;
@@ -129,4 +152,42 @@ equalsKey.addEventListener("click", ()=> {
 
     display.textContent = result;
     displayBuffer = String(display.textContent);
+});
+
+clearKey.addEventListener("click", () => {
+    displayBuffer = '';
+    display.textContent = displayBuffer;
+});
+
+backspaceKey.addEventListener("click", ()=> {
+    if (displayBuffer === '') return;
+    else {
+        displayBuffer = displayBuffer.slice(0, (displayBuffer.length -1));
+        display.textContent = displayBuffer;
+    }
+});
+
+pointKey.addEventListener("click", ()=> {
+    if(displayBuffer === '') {
+        displayBuffer = '0.';
+        display.textContent = displayBuffer;
+        return;
+    } else if(!displayBuffer.includes('.')) {
+        displayBuffer = displayBuffer + '.';
+        display.textContent = displayBuffer;
+        return;    
+    } else if (displayBuffer[0] !== '-' && operatorsRegex.test(displayBuffer)) {
+        operands = displayBuffer.split(operatorsRegex);
+        if(!operands[1].includes('.')) {
+            displayBuffer = displayBuffer + '.';
+            display.textContent = displayBuffer;
+            return;
+        }
+    } else if (operatorsRegex.test(displayBuffer.slice(1))) {
+        operands = displayBuffer.slice(1).split(operatorsRegex);
+        if(!operands[1].includes('.')) {
+            displayBuffer = displayBuffer + '.';
+            display.textContent = displayBuffer;
+        }
+    } 
 });
